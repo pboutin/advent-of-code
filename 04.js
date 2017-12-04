3f1debf84491c575a1ef63474e13e7294d23a2d2
Pascal Boutin <pboutin@mirego.com>
2017-12-04 08:57:34 -0500

    Solution for day 4

---
+++ 04.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('04_input.txt')
+                .toString()
+                .split('\n')
+                .filter((line) => line);
+
+/**
+ * Part I
+ ********************************/
+
+let validPasswords = input.filter((password) => {
+  const words = [];
+  const duplicatedWord = password.split(/ /g).find((word) => {
+     if (words.includes(word)) return true;
+     words.push(word);
+     return false;
+   });
+   return !duplicatedWord;
+});
+
+
+console.log('First output', validPasswords.length);
+
+/**
+ * Part II
+ ********************************/
+
+validPasswords = input.filter((password) => {
+  const words = [];
+  const duplicatedWord = password.split(/ /g).find((word) => {
+    const formattedWord = word.split('').sort().join('');
+    if (words.includes(formattedWord)) return true;
+    words.push(formattedWord);
+    return false;
+  });
+  return !duplicatedWord;
+});
+console.log('Second output', validPasswords.length);
