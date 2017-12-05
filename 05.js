64fc8257b1dfd613270dfa7f42668330a989516a
Pascal Boutin <pboutin@mirego.com>
2017-12-05 08:54:21 -0500

    Solution for day 5

---
+++ 05.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('05_input.txt')
+                .toString()
+                .split('\n')
+                .filter((line) => line)
+                .map(Number);
+
+/**
+ * Part I
+ ********************************/
+
+let currentIndex = 0;
+let stepsCount = 0;
+const partOneInput = input.slice(0);
+
+while (currentIndex >= 0 && currentIndex < partOneInput.length) {
+  stepsCount++;
+  const oldIndex = currentIndex;
+  currentIndex += partOneInput[currentIndex];
+  partOneInput[oldIndex]++;
+}
+
+
+console.log('First output', stepsCount);
+
+/**
+ * Part II
+ ********************************/
+
+currentIndex = 0;
+stepsCount = 0;
+const partTwoInput = input.slice(0);
+
+while (currentIndex >= 0 && currentIndex < partTwoInput.length) {
+  stepsCount++;
+  const oldIndex = currentIndex;
+  currentIndex += partTwoInput[currentIndex];
+  partTwoInput[oldIndex] += partTwoInput[oldIndex] >= 3 ? -1 : 1;
+}
+
+
+console.log('Second output', stepsCount);
